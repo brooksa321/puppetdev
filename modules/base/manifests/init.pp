be73f678521b1f0a11c90031e54a79e48dabef90
Aaron Brooks <Aaron Brooks>
2016-04-18 15:02:09 -0400

    Additionall Base development

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,6 +1,18 @@
+include freetds
+
 class base {
 
-	package{ 'httpd':
+	yum::group { 'Development Tools':
 		ensure => present,
 	}
+
+	#Setup Directory Structure
+	$server_dirs = [ '/usr1', '/usr2', '/usr3', '/usr4']
+
+	file { $server_dirs:
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
 }
